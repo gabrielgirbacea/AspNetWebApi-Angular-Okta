2eb521a1e77bc8abc990c8c7156c4e73edf5e886
Gabriel Girbacea <gabriel.girbacea@gmail.com>
2021-02-22 13:23:32 +0200

    Set a Default Formatter for ASP.NET Web API 2 to JSON
    Set a Default Formatter for ASP.NET Web API 2 to JSON

--- SugarLevelTracker/SugarLevelTracker/App_Start/WebApiConfig.cs
+++ SugarLevelTracker/SugarLevelTracker/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Http;
+﻿using Newtonsoft.Json.Serialization;
+using System.Web.Http;
 using System.Web.Http.Cors;
 
 namespace SugarLevelTracker
@@ -21,6 +22,12 @@ namespace SugarLevelTracker
             // Enable CORS for the Angular App
             var cors = new EnableCorsAttribute("http://localhost:4200", "*", "*");
             config.EnableCors(cors);
+
+            // Set JSON formatter as default one and remove XmlFormatter
+            var jsonFormatter = config.Formatters.JsonFormatter;
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            jsonFormatter.SerializerSettings.DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Utc;
         }
     }
 }
