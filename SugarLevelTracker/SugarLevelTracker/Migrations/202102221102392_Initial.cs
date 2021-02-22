4fe0dfd355aa39b03759f56335610f44b0441ca0
Gabriel Girbacea <gabriel.girbacea@gmail.com>
2021-02-22 13:17:55 +0200

    Analyze and Code Cleanup

--- SugarLevelTracker/SugarLevelTracker/Migrations/202102221102392_Initial.cs
+++ SugarLevelTracker/SugarLevelTracker/Migrations/202102221102392_Initial.cs
@@ -1,8 +1,7 @@
 namespace SugarLevelTracker.Migrations
 {
-    using System;
     using System.Data.Entity.Migrations;
-    
+
     public partial class Initial : DbMigration
     {
         public override void Up()
@@ -10,16 +9,16 @@ namespace SugarLevelTracker.Migrations
             CreateTable(
                 "dbo.SugarLevels",
                 c => new
-                    {
-                        Id = c.Int(nullable: false, identity: true),
-                        Value = c.Single(nullable: false),
-                        Description = c.String(nullable: false),
-                        MeasuredAt = c.DateTime(nullable: false),
-                    })
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    Value = c.Single(nullable: false),
+                    Description = c.String(nullable: false),
+                    MeasuredAt = c.DateTime(nullable: false),
+                })
                 .PrimaryKey(t => t.Id);
-            
+
         }
-        
+
         public override void Down()
         {
             DropTable("dbo.SugarLevels");
