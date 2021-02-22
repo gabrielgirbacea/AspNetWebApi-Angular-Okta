a479c000745512646b671487fa0611108f8a2643
Gabriel Girbacea <gabriel.girbacea@gmail.com>
2021-02-22 13:12:51 +0200

    Created first entity, SugarLevel. Created the DB context and the initial migration.
    Created first entity, SugarLevel. Created the DB context and the initial migration.

---
+++ SugarLevelTracker/SugarLevelTracker/Models/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+
+namespace SugarLevelTracker.Models.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("SugarLevelTrackerConnectionString")
+        {
+
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        public DbSet<SugarLevel> SugarLevels { get; set; }
+    }
+}
